afe8b34d1f882c4a8bef44475025496145537f85
zhengyanglim <2005224G@student.tp.edu.sg>
2022-01-26 15:30:58 +0800

    booking servlet done

---
+++ src/main/java/BookingServlet.java
@@ -0,0 +1,91 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BookingServlet
+ */
+@WebServlet("/BookingServlet")
+public class BookingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BookingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//Step 1: Initialize a PrintWriter object to return the html values via the response
+		PrintWriter out = response.getWriter();
+		
+		//Step 2: retrieve the four parameters from the request from the web form
+		String attractionid = request.getParameter("attractionid");
+		String attractionname = request.getParameter("attractionname");
+		String userid = request.getParameter("userid");
+		String date = request.getParameter("date");
+		String numberoftickets = request.getParameter("numberoftickets");
+		String totalprice = request.getParameter("totalprice");
+		
+		//Step 3: attempt connection to database using JDBC, you can change the username and password accordingly using the phpMyAdmin > User Account dashboard
+		try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 Connection con = DriverManager.getConnection(
+			 "jdbc:mysql://localhost:3306/bookingproject", "root", "");
+		
+		//Step 4: implement the sql query using prepared statement 
+		PreparedStatement ps = con.prepareStatement("insert into booking values(?,?,?,?,?,?)");
+		
+		//Step 5: parse in the data retrieved from the web form request into the prepared statement accordingly
+		 ps.setString(1, attractionid);
+		 ps.setString(2, attractionname);
+		 ps.setString(3, userid);
+		 ps.setString(4, date);
+		 ps.setString(5, numberoftickets);
+		 ps.setString(6, totalprice);
+		//Step 6: perform the query on the database using the prepared statement
+		 int i = ps.executeUpdate();
+
+		//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response,
+		  if (i > 0){
+			  PrintWriter writer = response.getWriter();
+			  writer.println("<h1>" + "You have successfully booked attraction's tickets!" + "</h1>");
+			  writer.close(); 
+			  } 
+			}
+		//Step 8: catch and print out any exception
+		catch (Exception exception) {
+			   System.out.println(exception);
+			   out.close();
+			  }
+		 
+		 // TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
--- src/main/webapp/booking.jsp
+++ src/main/webapp/booking.jsp
@@ -8,11 +8,12 @@
 </head>
 <body>
 	<form action="BookingServlet" method="post">
+		Attraction Id: <input type="text" name="attractionid">
 		Attraction Name: <input type="text" name="attractionname">
 		user: <input type="text" name="userid">
-		Date: <input type="text" name="date"> 
+		Date: <input type="date" name="date"> 
 		Number of Tickets: <input type="text" name="numberoftickets">
-		Total Cost: <input type="text" name="totalcost"> 
+		Total Cost: <input type="text" name="totalprice"> 
 		<input type="submit" value="Call Servlet" />
 	</form>
 </body>
