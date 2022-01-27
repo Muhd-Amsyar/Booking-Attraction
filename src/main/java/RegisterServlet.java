2241bcf340edee891bc2f8354a7c7917e74658fd
Amsyar <2004497j@student.tp.edu.sg>
2022-01-27 21:36:01 +0800

    Added new Java class file for updating and delete user. Updated servlet to redirect after login and register.

--- src/main/java/RegisterServlet.java
+++ src/main/java/RegisterServlet.java
@@ -62,13 +62,14 @@ public class RegisterServlet extends HttpServlet {
 		 ps.setString(4, n);
 		//Step 6: perform the query on the database using the prepared statement
 		 int i = ps.executeUpdate();
+		 response.sendRedirect("http://localhost:8090/BookingAttraction/login.jsp");
 		//Step 7: check if the query had been successfully execute, return “You are successfully registered” via the response,
-		 if (i > 0){
-		PrintWriter writer = response.getWriter();
-		writer.println("<h1>" + "You have successfully registered an account!" +
-		"</h1>");
-		writer.close();
-		}
+//		 if (i > 0){
+//		PrintWriter writer = response.getWriter();
+//		writer.println("<h1>" + "You have successfully registered an account!" +
+//		"</h1>");
+//		writer.close();
+//		}
 		}
 		//Step 8: catch and print out any exception
 		catch (Exception exception) {
